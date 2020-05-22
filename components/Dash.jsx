cf111d9df233030891ee3c96355d09e60d206ab3
marykellar <marymac@maryxmac.com>
2020-05-22 00:15:58 -0400

    updated pulling in user promise data to components

--- components/Dash.jsx
+++ components/Dash.jsx
@@ -1,10 +1,21 @@
 import Users from './shared/Users'
 import Feed from './shared/Feed'
+import { getMyTeamAndProjects} from '../public/helper/api-helper'
+import { useState, useEffect } from 'react'
+
 export default function Dash() {
+  
+  const [userData, setUserData] = useState([])
+  
+  useEffect(() => {
+    const data = getMyTeamAndProjects();
+    data.then((value) =>setUserData(value))
+    }, [])
+
     return(
         <div className="w-full mt-6 h-full text-center text-black font-f1">
             <h3 className="font-f1 text-left ml-4 tracking-wide">My Team</h3>
-            <Users />
+            <Users userData={userData} />
 
             <h3 className="font-f1 text-left mt-6 ml-4 tracking-wide">Latest News</h3>
             <Feed />
