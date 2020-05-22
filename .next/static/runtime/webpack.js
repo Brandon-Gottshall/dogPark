649dda310c3e7e6394190c18d7c3f20f537a0c1f
Brandon Gottshall <Brandon@gottshall.dev>
2020-05-22 09:16:50 -0400

    Changed the way slug is handled

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d4df583c59114fdc9ce0";
+/******/ 	var hotCurrentHash = "4b12dde8da31ba8ac1f8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/profile/[userID].jsx
+++ pages/profile/[userID].jsx
@@ -2,11 +2,12 @@ import '../../styles/tailwind.css'
 import { useRouter } from 'next/router'
 import SideNav from '../../components/SideNav'
 import CenterPane from '../../components/CenterPane'
-export default function Profile() {
-    const router = useRouter()
-    const { userID } = router.query
+export default function Profile({ userID }) {
+    Profile.getInitialProps = async ({ query }) => {
+      return { userID: query.userID }
+    }
     return(
-        <p>{userID}</p>
+        <p className='text-5xl text-center'>{userID}</p>
     )
 }
 {/* <div className="grid grid-cols-mobileHome sm:grid-cols-home grid-rows-1 h-screen overflow-y-hidden">
